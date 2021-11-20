e17d7de7b0f3660350a3587a233aed4f72a2516e
Alex M <alexmunoz91@gmail.com>
2021-11-20 22:52:56 +0100

    Add userController.createUser test

---
+++ backend/src/test/userController.test.js
@@ -0,0 +1,42 @@
+const userController = require('../controllers/userController');
+const User = require('../models/userModel');
+const userMock = require('../mocks/userMock');
+
+jest.mock('../models/userModel');
+
+let req;
+let res;
+
+describe('Given createUser function', () => {
+  beforeEach(() => {
+    req = { userMock };
+    res = { json: jest.fn(), status: jest.fn(), send: jest.fn() };
+  });
+  describe('When is invoked', () => {
+    describe('And User.create resolves', () => {
+      test('Then res.json is called', async () => {
+        User.create.mockResolvedValue({});
+
+        await userController.createUser(req, res);
+
+        expect(res.json).toHaveBeenCalled();
+      });
+    });
+    describe('And User.create rejects', () => {
+      test('Then res.status is called', async () => {
+        User.create.mockRejectedValue([]);
+
+        await userController.createUser(req, res);
+
+        expect(res.status).toHaveBeenCalledWith(500);
+      });
+      test('Then res.send is called with FIND_ERROR', async () => {
+        User.create.mockRejectedValue(new Error('FIND_ERROR'));
+
+        await userController.createUser(req, res);
+
+        expect(res.send.mock.calls[0][0].message).toBe('FIND_ERROR');
+      });
+    });
+  });
+});
