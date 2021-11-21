09fb3f98613e3e0dc3c5baa94f3c860e6bc3b4a1
Alex M <alexmunoz91@gmail.com>
2021-11-21 23:40:50 +0100

    Add getOneAlbumByid function

---
+++ backend/src/controllers/album/index.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+const Album = require('../../models/album');
+
+async function getAlbums(req, res) {
+  try {
+    const { data } = await axios.get(`${process.env.API_URL}albums${req.url}`);
+    res.json(data);
+  } catch (error) {
+    res.status(500);
+    res.send(error);
+  }
+}
+
+async function getOneAlbumById(req, res) {
+  try {
+    const { data } = await axios.get(`${process.env.API_URL}albums${req.url}`);
+    res.json(data);
+  } catch (error) {
+    res.status(500);
+    res.send(error);
+  }
+}
+
+module.exports = {
+  getAlbums,
+  getOneAlbumById
+};
