fdafa1d3c20daa90c8b3a7ec682411ad80f4ee09
Alex M <alexmunoz91@gmail.com>
2021-11-21 23:41:58 +0100

    Add getOneALbumById controller route

--- backend/src/routes/album.js
+++ backend/src/routes/album.js
@@ -1,13 +1,13 @@
 const { Router } = require('express');
-const { getAlbums } = require('../controllers/album');
-const { getPhotos } = require('../controllers/photo');
+const { getAlbums, getOneAlbumById } = require('../controllers/album');
+const { getAlbumPhotos } = require('../controllers/album/photos');
 
 const albumRouter = new Router();
 
 albumRouter.route('/').get(getAlbums);
 
-albumRouter.route('/:albumId').get(getAlbums);
+albumRouter.route('/:albumId').get(getOneAlbumById);
 
-albumRouter.route('/:albumId/photos').get(getPhotos);
+albumRouter.route('/:albumId/photos').get(getAlbumPhotos);
 
 module.exports = albumRouter;
