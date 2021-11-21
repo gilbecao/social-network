0d76b952e7007296a34295bac890a415dcf1a637
Gilberto Cao <gilbe.cao@gmail.com>
2021-11-21 21:30:23 +0100

    Add posts route, model and controller

---
+++ backend/src/controllers/post.js
@@ -0,0 +1,64 @@
+const axios = require('axios');
+const Post = require('../models/post');
+
+const postUrl = `${process.env.API_URL}posts`;
+
+async function getPosts(req, res) {
+  try {
+    const { data } = await axios.get(`${process.env.API_URL}posts${req.url}`);
+    res.json(data);
+  } catch (error) {
+    res.status(500);
+    res.send(error);
+  }
+}
+
+async function createPost(req, res) {
+  try {
+    const data = req.body;
+    const response = await axios.post(`${postUrl}`, data, {
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8'
+      }
+    });
+
+    res.json(response.data);
+  } catch (error) {
+    res.status(500);
+    res.send(error);
+  }
+}
+
+async function updatePost(req, res) {
+  try {
+    const { postId } = req.params;
+    const data = req.body;
+    const response = await axios.put(`${postUrl}/${postId}`, data, {
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8'
+      }
+    });
+    res.json(response.data);
+  } catch (error) {
+    res.status(500);
+    res.send(error);
+  }
+}
+
+async function deletePost(req, res) {
+  try {
+    const { postId } = req.params;
+    await axios.delete(`${postUrl}/${postId}`);
+    res.send();
+  } catch (error) {
+    res.status(500);
+    res.send(error);
+  }
+}
+
+module.exports = {
+  createPost,
+  getPosts,
+  updatePost,
+  deletePost
+};
