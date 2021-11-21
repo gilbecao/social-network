fa88c57e21328d9ee973ebd9a57dec883ff40108
erasmogarcia84 <erasmogarciaglez@gmail.com>
2021-11-21 22:17:30 +0100

    Add routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { PostCreateComponent } from './pages/post-create/post-create.component';
+import { PostDetailsComponent } from './pages/post-details/post-details.component';
+import { PostEditComponent } from './pages/post-edit/post-edit.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'post/:id', component: PostDetailsComponent },
+  { path: 'create', component: PostCreateComponent },
+  { path: 'edit', component: PostEditComponent },
+  { path: '**', redirectTo: '/home' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
