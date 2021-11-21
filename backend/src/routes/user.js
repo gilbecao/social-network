012431fb3aeb946be4c44104b84151eb236c15ac
Gilberto Cao <gilbe.cao@gmail.com>
2021-11-21 22:09:44 +0100

    Add misc routes

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,8 +1,13 @@
 const { Router } = require('express');
-const { createUser } = require('../controllers/user');
+
+const albumsController = (req, res) => res.send('user albums works');
+const todosController = (req, res) => res.send('user todos works');
+const postsController = (req, res) => res.send('user posts works');
 
 const userRouter = new Router();
 
-userRouter.route('/signup').post(createUser);
+userRouter.route('/:userId/albums').get(albumsController);
+userRouter.route('/:userId/todos').get(todosController);
+userRouter.route('/:userId/posts').get(postsController);
 
 module.exports = userRouter;
