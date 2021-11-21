37547e1ab5c13dde342224fc8f2420d662d3cefc
Gilberto Cao <gilbe.cao@gmail.com>
2021-11-21 21:51:19 +0100

    Add post controller tests

--- backend/src/__test__/postController.test.js
+++ backend/src/__test__/postController.test.js
@@ -59,4 +59,169 @@ describe('Given controllers/post', () => {
       expect(res.send).toHaveBeenCalledWith(error);
     });
   });
+
+  describe('When createPost resolves', () => {
+    test('Then respond with {userId, title and body}', async () => {
+      const response = {
+        data: {
+          userId: 1,
+          title: 'title',
+          body: 'body'
+        }
+      };
+
+      const req = {
+        body: {
+          userId: 1,
+          title: 'title',
+          body: 'body'
+        }
+      };
+      const res = {
+        json: jest.fn()
+      };
+
+      axios.post.mockResolvedValue(response);
+
+      await createPost(req, res);
+
+      expect(res.json).toHaveBeenCalledWith(response.data);
+    });
+  });
+
+  describe('When createPost rejects', () => {
+    test('Then respond with 500', async () => {
+      const error = {
+        message: 'error'
+      };
+
+      const req = {
+        body: {
+          userId: 1,
+          title: 'title',
+          body: 'body'
+        }
+      };
+      const res = {
+        json: jest.fn(),
+        status: jest.fn(),
+        send: jest.fn()
+      };
+
+      axios.post.mockRejectedValue(error);
+
+      await createPost(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(500);
+      expect(res.send).toHaveBeenCalledWith(error);
+    });
+  });
+
+  describe('When updatePost resolves', () => {
+    test('Then respond with {userId, title and body}', async () => {
+      const response = {
+        data: {
+          userId: 1,
+          title: 'title',
+          body: 'body'
+        }
+      };
+
+      const req = {
+        params: {
+          postId: 1
+        },
+        body: {
+          userId: 1,
+          title: 'title',
+          body: 'body'
+        }
+      };
+      const res = {
+        json: jest.fn()
+      };
+
+      axios.put.mockResolvedValue(response);
+
+      await updatePost(req, res);
+
+      expect(res.json).toHaveBeenCalledWith(response.data);
+    });
+  });
+
+  describe('When updatePost rejects', () => {
+    test('Then respond with 500', async () => {
+      const error = {
+        message: 'error'
+      };
+
+      const req = {
+        params: {
+          postId: 1
+        },
+        body: {
+          userId: 1,
+          title: 'title',
+          body: 'body'
+        }
+      };
+      const res = {
+        json: jest.fn(),
+        status: jest.fn(),
+        send: jest.fn()
+      };
+
+      axios.put.mockRejectedValue(error);
+
+      await updatePost(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(500);
+      expect(res.send).toHaveBeenCalledWith(error);
+    });
+  });
+
+  describe('When deletePost resolves', () => {
+    test('Then respond with {userId, title and body}', async () => {
+      const req = {
+        params: {
+          postId: 1
+        }
+      };
+      const res = {
+        json: jest.fn(),
+        send: jest.fn(),
+        status: jest.fn()
+      };
+
+      await deletePost(req, res);
+
+      expect(res.send).toHaveBeenCalled();
+    });
+  });
+
+  describe('When deletePost rejects', () => {
+    test('Then respond with 500', async () => {
+      const error = {
+        message: 'error'
+      };
+
+      const req = {
+        params: {
+          postId: 1
+        }
+      };
+      const res = {
+        json: jest.fn(),
+        status: jest.fn(),
+        send: jest.fn()
+      };
+
+      axios.delete.mockRejectedValue(error);
+
+      await deletePost(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(500);
+      expect(res.send).toHaveBeenCalledWith(error);
+    });
+  });
 });
