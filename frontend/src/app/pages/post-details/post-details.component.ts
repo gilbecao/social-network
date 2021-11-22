ba0d6d93539d0f419ff782b8bcf7eceda2c68f3f
erasmogarcia84 <erasmogarciaglez@gmail.com>
2021-11-22 03:19:27 +0100

    Add content to Post-details

--- frontend/src/app/pages/post-details/post-details.component.ts
+++ frontend/src/app/pages/post-details/post-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { PostService } from 'src/app/core/services/post/post.service';
 
 @Component({
   selector: 'app-post-details',
@@ -6,7 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-details.component.scss'],
 })
 export class PostDetailsComponent implements OnInit {
-  constructor() {}
+  public post = {
+    userId: 1,
+    id: 1,
+    title:
+      'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+    body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
+  };
 
-  ngOnInit(): void {}
+  constructor(
+    private route: ActivatedRoute,
+    private postService: PostService
+  ) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      console.log(params['id']);
+    });
+  }
 }
