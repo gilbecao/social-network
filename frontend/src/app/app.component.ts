4247ef560b71e82a376dcd85bb105e31493be8e9
Gilberto Cao <gilbe.cao@gmail.com>
2021-11-22 01:01:35 +0100

    Add post list to handle posts subscription

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'Coders Network';
-}
+export class AppComponent {}
--- frontend/src/app/components/post-list/post-list.component.spec.ts
+++ frontend/src/app/components/post-list/post-list.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { PostService } from 'src/app/core/services/post/post.service';
 
 import { PostListComponent } from './post-list.component';
 
@@ -6,11 +8,15 @@ describe('PostListComponent', () => {
   let component: PostListComponent;
   let fixture: ComponentFixture<PostListComponent>;
 
+  const postServiceStub = {
+    fetchPosts: () => of([]),
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PostListComponent ]
-    })
-    .compileComponents();
+      declarations: [PostListComponent],
+      providers: [{ provide: PostService, useValue: postServiceStub }],
+    }).compileComponents();
   });
 
   beforeEach(() => {
