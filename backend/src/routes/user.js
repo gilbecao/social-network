b5b11e8619a51f8f5ec88a6ccc0bdfc9ecab752f
pablongo <paugallardocordon@gmail.com>
2021-11-22 01:09:57 +0100

    Refactor user router

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,13 +1,13 @@
 const { Router } = require('express');
 
-const albumsController = (req, res) => res.send('user albums works');
-const todosController = (req, res) => res.send('user todos works');
-const postsController = (req, res) => res.send('user posts works');
+const { getAlbums } = require('../controllers/user/album');
+const { getTodos } = require('../controllers/user/todo');
+const { getPosts } = require('../controllers/user/post');
 
 const userRouter = new Router();
 
-userRouter.route('/:userId/albums').get(albumsController);
-userRouter.route('/:userId/todos').get(todosController);
-userRouter.route('/:userId/posts').get(postsController);
+userRouter.route('/:userId/albums').get(getAlbums);
+userRouter.route('/:userId/todos').get(getTodos);
+userRouter.route('/:userId/posts').get(getPosts);
 
 module.exports = userRouter;
