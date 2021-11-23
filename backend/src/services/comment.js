84a3e66b41e5e08f7504c0d6ca48824377387d0c
Alex M <alexmunoz91@gmail.com>
2021-11-23 23:12:09 +0100

    Add createComment Service

--- backend/src/services/comment.js
+++ backend/src/services/comment.js
@@ -1,9 +1,14 @@
 const Comment = require('../models/comment');
 
+function createComment(body) {
+  return Comment.create(body);
+}
+
 function getComments(query) {
   return Comment.find(query);
 }
 
 module.exports = {
+  createComment,
   getComments,
 };
