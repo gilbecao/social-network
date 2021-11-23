a13433b0804eb3dbe26261f6a231ed1f39f805fc
Alex M <alexmunoz91@gmail.com>
2021-11-23 23:18:07 +0100

    Modify user routes

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -1,5 +1,11 @@
 const { Router } = require('express');
-const { getUsers, getUserById, createUser } = require('../controllers/user');
+const {
+  getUsers,
+  createUser,
+  getUserById,
+  updateUserById,
+  deleteUserById,
+} = require('../controllers/user');
 
 const { getAlbums } = require('../controllers/user/album');
 const { getTodos } = require('../controllers/user/todo');
@@ -8,7 +14,13 @@ const { getPosts } = require('../controllers/user/post');
 const router = new Router();
 
 router.route('/').get(getUsers).post(createUser);
-router.route('/:userId').get(getUserById);
+
+router
+  .route('/:userId')
+  .get(getUserById)
+  .put(updateUserById)
+  .delete(deleteUserById);
+
 router.route('/:userId/albums').get(getAlbums);
 router.route('/:userId/todos').get(getTodos);
 router.route('/:userId/posts').get(getPosts);
