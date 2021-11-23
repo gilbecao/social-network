4027fce3f1f6ab9dd69148ec7cdbe65291335d2c
Alex M <alexmunoz91@gmail.com>
2021-11-23 23:16:11 +0100

    Refactor post controller functions

--- backend/src/controllers/post.js
+++ backend/src/controllers/post.js
@@ -1,37 +1,9 @@
-const axios = require('axios');
-const Post = require('../models/post');
 const commentService = require('../services/comment');
-
-const postUrl = `${process.env.API_URL}/posts`;
-
-function mapPostAuthor(posts) {
-  const promises = posts.map((post) =>
-    axios.get(`${process.env.API_URL}/users/${post.userId}`)
-  );
-
-  return Promise.all(promises);
-}
-
-async function getPosts(req, res) {
-  try {
-    const { data } = await axios.get(`${postUrl}${req.url}`);
-    const authors = await mapPostAuthor(data);
-
-    const postData = data.map((post, index) => ({
-      ...post,
-      author: authors[index].data,
-    }));
-
-    res.json(postData);
-  } catch (error) {
-    res.status(500);
-    res.send(error);
-  }
-}
+const postService = require('../services/post');
 
 async function createPost({ body }, res) {
   try {
-    const newPost = await Post.create(body);
+    const newPost = await postService.createPost(body);
     res.json(newPost);
   } catch (error) {
     res.status(500);
@@ -39,26 +11,41 @@ async function createPost({ body }, res) {
   }
 }
 
-async function updatePost(req, res) {
+async function getPosts(req, res) {
   try {
-    const { postId } = req.params;
-    const data = req.body;
-    const response = await axios.put(`${postUrl}/${postId}`, data, {
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
-    });
-    res.json(response.data);
+    const response = await postService.getPosts();
+    res.json(response);
   } catch (error) {
     res.status(500);
     res.send(error);
   }
 }
 
-async function deletePost(req, res) {
+async function getPostById(req, res) {
   try {
     const { postId } = req.params;
-    await axios.delete(`${postUrl}/${postId}`);
+    const response = await postService.getPostById(postId);
+    res.json(response);
+  } catch (error) {
+    res.status(500);
+    res.send(error);
+  }
+}
+
+async function updatePostById({ body, params: { postId } }, res) {
+  try {
+    const response = await postService.updatePostById(postId, body);
+    res.json(response);
+  } catch (error) {
+    res.status(500);
+    res.send(error);
+  }
+}
+
+async function deletePostById(req, res) {
+  try {
+    const { postId } = req.params;
+    await postService.deletePostById(postId);
     res.send();
   } catch (error) {
     res.status(500);
@@ -80,7 +67,8 @@ async function getPostComments(req, res) {
 module.exports = {
   createPost,
   getPosts,
-  updatePost,
-  deletePost,
+  getPostById,
+  updatePostById,
+  deletePostById,
   getPostComments,
 };
