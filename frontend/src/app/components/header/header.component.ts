e16361c0c400e3b1a51bd1d78c5e83123e6cac11
erasmogarcia84 <erasmogarciaglez@gmail.com>
2021-11-24 09:05:07 +0100

    Add themes switch

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -3,13 +3,30 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  public darkMode: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    if (
+      window.matchMedia &&
+      window.matchMedia('(prefers-color-scheme: dark)').matches
+    ) {
+      this.darkMode = true;
+      document.documentElement.setAttribute('data-color-theme', 'dark');
+    } else {
+      this.darkMode = false;
+      document.documentElement.setAttribute('data-color-theme', 'light');
+    }
   }
 
+  public switchTheme(): void {
+    this.darkMode = !this.darkMode;
+    this.darkMode
+      ? document.documentElement.setAttribute('data-color-theme', 'dark')
+      : document.documentElement.setAttribute('data-color-theme', 'light');
+  }
 }
